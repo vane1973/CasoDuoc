b8ee07ab6a3e342207a62eec5ce84011e8d08f7c
JuanCarlos <jcreyes@ferochi.cl>
2024-09-08 13:21:24 -0300

    Se hicieron las siguientes modificaciones: Page acceso, titulo y configuracion de botones Page home, titulo y redireccion de botones Restablecer page; configuracion de botones restableces css; modificaciones visuales

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage implements OnInit {
+  loading = true; // El estado de carga se inicia en verdadero
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.loading = true; // Inicia la barra de carga al empezar la navegación
+      } else if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          this.loading = false; // Desactiva la barra de carga después de cargar
+        }, 1000); // Puedes ajustar el tiempo según sea necesario
+      }
+    });
+  }
 }
+
